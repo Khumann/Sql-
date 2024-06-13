de22e41509252de44c163f7f4eae945b621fab20
Shubham <chaudhary.shubham812@gmail.com>
2024-06-13 11:10:00 +0100

    updated query

--- sql-practice.sql
+++ sql-practice.sql
@@ -138,3 +138,37 @@ from ( select d.name as department, E.name as employee, salary,
          inner join Department D on E.departmentid = D.id) as t1
 
 where Rak <=3
+
+
+/*You are given two tables: Students and Grades. Students contains three columns ID, Name and Marks.
+
+
+
+Grades contains the following data:
+
+
+
+Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. Ketty doesn't want the NAMES of those students who received a grade lower than 8. The report must be in descending order by grade -- i.e. higher grades are entered first. If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order. If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
+
+Write a query to help Eve.*/
+
+SELECT 
+    CASE 
+        WHEN G.grade < 8 THEN 'NULL' 
+        ELSE S.name 
+    END AS Name, 
+    G.grade AS Grade, 
+    S.marks AS Marks
+FROM 
+    Students S 
+INNER JOIN 
+    Grades G 
+ON 
+    S.marks BETWEEN G.min_mark AND G.max_mark
+ORDER BY 
+    G.grade DESC,
+    CASE 
+        WHEN G.grade < 8 THEN S.marks
+        ELSE S.name
+    END,
+    S.marks;
